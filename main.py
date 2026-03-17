e69f50e2088be84858752d8ab9ee8a70b1d69f05
TERRibledefine <3690394898@qq.com>
2026-03-17 09:04:42 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -97,8 +97,8 @@ def main(users, action=False):
             break
         time.sleep(0.5)
         target_wait=target_wait+1
-        if(target_wait%10=0)
-            logging.infor("wait ")
+        if(target_wait%10==0)
+            logging.info("wait ")
 
     logging.info("时间到！开始抢座！")
     # ==========================================================================================
